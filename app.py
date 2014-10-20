112e244e9fd9e433a6e3c29bbba743519caaef5f
Callum Smith <callum1234moo@gmail.com>
2014-10-20 02:53:09 -0400

    playing with some ideas

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from bottle import *
 import praw
 
+def posts():	
+	posts = r.get_subreddit('askreddit').get_top(params={'t': 'month'}, limit=5)
+	#posts = [x for x in posts]
+	#avg = reduce(lambda x, y: x + y, [x.score for x in posts]) / 50
+	c = [str(x.score) + x.title + "<br>" + str([y.body + "<br>" for y in x.comments[0:5] if y.body ]) + "<br><br>" for x in posts]
+	print "done"
+	return c
+
 app = Bottle()
 r = praw.Reddit(user_agent='the redditor v1')
+results = posts()
 
 @app.route('/')
 def root():
-	return posts()
-
-def posts():	
-	posts = r.get_subreddit('askreddit').get_top(params={'t': 'month'}, limit=50)
-	#return [x.selftext + "\n<br><br>" for x in posts]
-	posts = [x for x in posts]
-	avg = reduce(lambda x, y: x + y, [x.score for x in posts]) / 50
-	return [str(x.score) + x.title +"<br>"+ x.selftext + "\n<br><br>" for x in posts if x.score > avg]
+	return results
 
 app.run(port="8080")
