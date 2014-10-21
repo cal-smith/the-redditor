60dce60d009824a4d67750d4e4058631ec222793
Callum Smith <callum1234moo@gmail.com>
2014-10-21 02:14:25 -0400

    got some react going

--- static/app.js
+++ static/app.js
@@ -1,9 +1,60 @@
-ajax.send({url:"/json", json: true}, function (data) {
-	var elm = document.getElementById('issue');
-	for (var i = 0; i < data.length; i++) {
-		elm.innerHTML += "<h4>" + data[i].title + "</h4>";
-		for (var x = 0; x < data[i].comments.length; x++) {
-			elm.innerHTML += "<p>" + data[i].comments[x].body + "</p>";
-		}
+/** @jsx React.DOM */
+
+var Post = React.createClass({
+	load: function(){
+		ajax.send({url: '/json', json: true}, function(data){
+			this.setState({data: data});
+		}.bind(this));
+	},
+	getInitialState: function(){
+		return {data: []};
+	},
+	componentDidMount: function(){
+		this.load();
+	},
+	render: function(){
+		var nodes = this.state.data.map(function(posts){
+			return (
+				<div className="post">
+					<h4>{posts.title}</h4>
+					<Comments data={posts.comments}></Comments>
+				</div>
+			)
+		});
+		return (
+			<div>
+				{nodes}
+			</div>
+		)
 	}
 });
+
+var Comments = React.createClass({
+	render: function(){
+		var nodes = this.props.data.map(function(comment){
+			return (
+				<p>{comment.body}</p>
+			)
+		});
+		return (
+			<div className="comments">
+				{nodes}
+			</div>
+		);
+	}
+});
+
+/*var comment = React.createClass({ I'll have use for you yet...
+	render: function(){
+		return (
+			<p className="comment">
+				comment
+			</p>
+		);
+	}
+});*/
+
+React.renderComponent(
+	<Post />,
+	document.getElementById('issue')
+)
